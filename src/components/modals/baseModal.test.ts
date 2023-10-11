7cb5f8d1a51036846111876c50e92842a8957bad
Rayna Ivanova <rivanova@sorint.com>
2023-10-11 12:22:57 +0200

    remove condition it

--- src/components/modals/baseModal.test.ts
+++ src/components/modals/baseModal.test.ts
@@ -1,21 +1,21 @@
 import { mount } from '@vue/test-utils';
 import baseModal from './baseModal.vue';
 
-test('baseModal', () => {
-  it('renders modal and handles focus trapping', async () => {
-    const wrapper = mount(baseModal, {
-      props: { show: true },
-      global: {
-        stubs: {
-          teleport: true,
-        },
+test('baseModal', async () => {
+  // it('renders modal and handles focus trapping', async () => {
+  const wrapper = mount(baseModal, {
+    props: { show: true },
+    global: {
+      stubs: {
+        teleport: true,
       },
-    });
-    vi.useFakeTimers();
-    await wrapper.find('[tabindex="0"]').trigger('keydown', { key: 'Escape' });
-    vi.runAllTicks();
-    const dismissEvent = wrapper.emitted('dismiss')!;
-    expect(dismissEvent).toHaveLength(1);
-    expect(dismissEvent[0]).toEqual([false]);
+    },
   });
+  vi.useFakeTimers();
+  await wrapper.find('[tabindex="0"]').trigger('keydown', { key: 'Escape' });
+  vi.runAllTicks();
+  const dismissEvent = wrapper.emitted('dismiss')!;
+  expect(dismissEvent).toHaveLength(1);
+  expect(dismissEvent[0]).toEqual([false]);
+  // });
 });
