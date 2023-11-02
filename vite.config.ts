4440b0eab767d1b625c7b62f513ec7088f067900
Rayna Ivanova <rivanova@sorint.com>
2023-11-02 09:07:03 +0100

    test sg

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/mocks/server.js'],
+    setupFiles: ['./tests/setup-router-mock.ts'],
   },
 });
